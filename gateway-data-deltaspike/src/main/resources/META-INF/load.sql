c6d05ab6348bcbc3724a56aec391bcc30b0ae04b
Paul Campbell <pcampbell@kemitix.net>
2018-08-13 22:31:11 +0100

    gateway-data-deltaspike: update sql

--- gateway-data-deltaspike/src/main/resources/META-INF/load.sql
+++ gateway-data-deltaspike/src/main/resources/META-INF/load.sql
@@ -1,3 +1,7 @@
+insert into vet_specialisations values ('RADIOLOGY');
+insert into vet_specialisations values ('DENTISTRY');
+insert into vet_specialisations values ('SURGERY');
+
 insert into veterinarians values (1, 'Conah Feeney');
 insert into veterinarians values (2, 'Austin Santiago');
 insert into veterinarians values (3, 'Violet Holmes');
@@ -9,14 +13,13 @@ insert into veterinarians values (8, 'Livia Wilkins');
 insert into veterinarians values (9, 'Calum Langley');
 insert into veterinarians values (10, 'Rafael Mackenzie');
 
-insert into veterinarian_specialisations values (1, 'RADIOLOGY');
-insert into veterinarian_specialisations values (4, 'RADIOLOGY');
-insert into veterinarian_specialisations values (6, 'RADIOLOGY');
-insert into veterinarian_specialisations values (9, 'RADIOLOGY');
+insert into veterinarians_vet_specialisations values (1, 'RADIOLOGY');
+insert into veterinarians_vet_specialisations values (4, 'RADIOLOGY');
+insert into veterinarians_vet_specialisations values (6, 'RADIOLOGY');
+insert into veterinarians_vet_specialisations values (9, 'RADIOLOGY');
+insert into veterinarians_vet_specialisations values (2, 'DENTISTRY');
+insert into veterinarians_vet_specialisations values (3, 'DENTISTRY');
+insert into veterinarians_vet_specialisations values (9, 'DENTISTRY');
 
-insert into veterinarian_specialisations values (2, 'DENTISTRY');
-insert into veterinarian_specialisations values (3, 'DENTISTRY');
-insert into veterinarian_specialisations values (9, 'DENTISTRY');
-
-insert into veterinarian_specialisations values (3, 'SURGERY');
-insert into veterinarian_specialisations values (9, 'SURGERY');
+insert into veterinarians_vet_specialisations values (3, 'SURGERY');
+insert into veterinarians_vet_specialisations values (9, 'SURGERY');
