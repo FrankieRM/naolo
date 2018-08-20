3d78ed21c79778cef376fffc831be1daec0e5e9c
Paul Campbell <pcampbell@kemitix.net>
2018-08-20 07:19:09 +0100

    run-meecrowave-*: add usecase tests

---
+++ run-meecrowave-jpa/src/test/java/net/kemitix/naolo/run/meecrowave/jpa/MeecrowaveJPAUseCasesTest.java
@@ -0,0 +1,36 @@
+package net.kemitix.naolo.run.meecrowave.jpa;
+
+import net.kemitix.naolo.core.UseCase;
+import net.kemitix.naolo.core.VeterinarianAdd;
+import net.kemitix.naolo.core.VeterinarianRepository;
+import net.kemitix.naolo.core.VeterinariansListAll;
+import net.kemitix.naolo.test.UseCasesTest;
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class MeecrowaveJPAUseCasesTest implements WithAssertions, UseCasesTest {
+
+    private final UseCases useCases = new UseCases();
+
+    private final VeterinarianRepository veterinarianRepository = mock(VeterinarianRepository.class);
+
+    @Override
+    @Test
+    public void hasListAllVeterinariansUseCase() {
+        //given
+        final VeterinariansListAll useCase = useCases.veterinariansListAll(veterinarianRepository);
+        //then
+        assertThat(useCase).isInstanceOf(UseCase.class);
+    }
+
+    @Override
+    @Test
+    public void hasAddVeterinarianUseCase() {
+        //given
+        final VeterinarianAdd useCase = useCases.veterinarianAdd(veterinarianRepository);
+        //then
+        assertThat(useCase).isInstanceOf(UseCase.class);
+    }
+}
--- run-spring-boot/src/test/java/net/kemitix/naolo/run/spring/SpringBootUseCasesTest.java
+++ run-spring-boot/src/test/java/net/kemitix/naolo/run/spring/SpringBootUseCasesTest.java
@@ -4,27 +4,30 @@ import net.kemitix.naolo.core.UseCase;
 import net.kemitix.naolo.core.VeterinarianAdd;
 import net.kemitix.naolo.core.VeterinarianRepository;
 import net.kemitix.naolo.core.VeterinariansListAll;
+import net.kemitix.naolo.test.UseCasesTest;
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.mock;
 
-class UseCasesTest implements WithAssertions {
+class SpringBootUseCasesTest implements WithAssertions, UseCasesTest {
 
     private final UseCases useCases = new UseCases();
 
     private final VeterinarianRepository veterinarianRepository = mock(VeterinarianRepository.class);
 
+    @Override
     @Test
-    void hasListAllVeterinariansUseCase() {
+    public void hasListAllVeterinariansUseCase() {
         //given
         final VeterinariansListAll useCase = useCases.veterinariansListAll(veterinarianRepository);
         //then
         assertThat(useCase).isInstanceOf(UseCase.class);
     }
 
+    @Override
     @Test
-    void hasAddVeterinarianUseCase() {
+    public void hasAddVeterinarianUseCase() {
         //given
         final VeterinarianAdd useCase = useCases.veterinarianAdd(veterinarianRepository);
         //then
