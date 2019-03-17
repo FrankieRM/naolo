fc7851e670a8c142cbad7ce4c308b43e15ff4646
Paul Campbell <pcampbell@kemitix.net>
2019-03-17 20:02:57 +0000

    run-meecrowave-jpa: Add MeecrowaveJPSRunIT

---
+++ run-meecrowave-jpa/src/test/java/net/kemitix/naolo/run/meecrowave/jpa/MeecrowaveJPARunIT.java
@@ -0,0 +1,36 @@
+package net.kemitix.naolo.run.meecrowave.jpa;
+
+import net.kemitix.naolo.test.AbstractMeecrowaveTest;
+import net.kemitix.naolo.test.RunIT;
+import org.apache.meecrowave.Meecrowave;
+import org.apache.meecrowave.junit5.MeecrowaveConfig;
+import org.apache.meecrowave.testing.ConfigurationInject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+@MeecrowaveConfig()
+public class MeecrowaveJPARunIT extends AbstractMeecrowaveTest implements RunIT {
+
+    @ConfigurationInject
+    private Meecrowave.Builder config;
+
+    @BeforeEach
+    void setUp() {
+        super.config(config);
+    }
+
+    @Test
+    @Override
+    public void getAllVets() throws IOException {
+        doGetAllVetsTest();
+    }
+
+    @Test
+    @Override
+    public void addVet() throws IOException {
+        doAddVetTest();
+    }
+
+}
